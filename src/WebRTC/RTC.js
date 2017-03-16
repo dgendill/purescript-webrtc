8e162d37fc5e55adeeb94db1259a75d05ccfd88a
Dominick Gendill <dgendill@gmail.com>
2017-03-15 20:35:30 -0600

    Experimenting with some changes

---
+++ src/WebRTC/RTC.js
@@ -0,0 +1,165 @@
+// module WebRTC.RTC
+
+exports.newRTCPeerConnection_ = function(ice) {
+    return function() {
+        return new (window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection)(ice, {
+            optional: [
+		            { DtlsSrtpKeyAgreement: true }
+	          ]
+        });
+    };
+};
+
+exports.addStream = function(stream) {
+    return function(pc) {
+        return function() {
+            pc.addStream(stream);
+        };
+    };
+};
+
+exports.rtcSessionDescriptionToJson = function(rtcSessionDescription) {
+  console.log(rtcSessionDescription);
+  return rtcSessionDescription.toJSON();
+}
+
+exports.onicecandidate = function(f) {
+    return function(pc) {
+        return function() {
+            pc.onicecandidate = function(event) {
+                f(event)();
+            };
+        };
+    };
+};
+
+exports.onaddstream = function(f) {
+    return function(pc) {
+        return function() {
+            pc.onaddstream = function(event) {
+                f(event)();
+            };
+        };
+    };
+};
+
+exports._createOffer = function(success) {
+    return function(error) {
+        return function(pc) {
+            return function() {
+                pc.createOffer(
+                    function(desc) {
+                        success(desc)();
+                    },
+                    function(e) {
+                        error(e)();
+                    }
+                );
+            };
+        };
+    };
+};
+
+exports._createAnswer = function(success) {
+    return function(error) {
+        return function(pc) {
+            return function() {
+                pc.createAnswer(
+                    function(desc) {
+                        success(desc)();
+                    },
+                    function(e) {
+                        error(e)();
+                    }
+                );
+            };
+        };
+    };
+};
+
+exports._setLocalDescription = function(success) {
+    return function(error) {
+        return function(desc) {
+            return function(pc) {
+                return function() {
+                    pc.setLocalDescription(
+                        desc,
+                        success,
+                        function(e) {
+                            error(e)();
+                        }
+                    );
+                };
+            };
+        };
+    };
+};
+
+exports._setRemoteDescription = function(success) {
+    return function(error) {
+        return function(desc) {
+            return function(pc) {
+                return function() {
+                    pc.setRemoteDescription(
+                        desc,
+                        success,
+                        function(e) {
+                            error(e)();
+                        }
+                    );
+                };
+            };
+        };
+    };
+};
+
+exports._iceEventCandidate = function(nothing) {
+    return function(just) {
+        return function(e) {
+            return e.candidate ? just(e.candidate) : nothing;
+        };
+    };
+};
+
+exports.addIceCandidate = function(c) {
+    return function(pc) {
+        return function() {
+            pc.addIceCandidate(new RTCIceCandidate(c));
+        };
+    };
+};
+
+exports.newRTCSessionDescription = function(s) {
+    return new RTCSessionDescription(s);
+};
+
+exports.createDataChannel = function(s) {
+    return function(pc) {
+        return function() {
+            var dc = pc.createDataChannel(s);
+            dc.onopen = function() {
+                console.log("OPENED");
+            }
+            return dc;
+        };
+    };
+};
+
+exports.send = function(s) {
+    return function(dc) {
+        return function() {
+            if (dc.readyState != "open") return;
+            dc.send(s);
+        };
+    };
+};
+
+exports.onmessageChannel = function(f) {
+    return function(dc) {
+        return function() {
+            dc.onmessage = function(m) {
+                f(m)();
+            };
+        };
+    };
+};
---
+++ src/WebRTC/RTC.purs
@@ -0,0 +1,168 @@
+module WebRTC.RTC (
+  RTCPeerConnection(..)
+, RTCSessionDescription(..)
+, Ice(..)
+, IceEvent(..)
+, MediaStreamEvent(..)
+, RTCIceCandidate(..)
+, RTCDataChannel(..)
+, ServerType(..)
+, newRTCPeerConnection
+, addStream
+, onicecandidate
+, onaddstream
+, createOffer
+, createAnswer
+, setLocalDescription
+, setRemoteDescription
+, newRTCSessionDescription
+, iceEventCandidate
+, addIceCandidate
+, createDataChannel
+, send
+, onmessageChannel
+, rtcSessionDescriptionToString
+) where
+
+import WebRTC.MediaStream
+import Control.Monad.Aff (Aff, makeAff)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Exception (Error)
+import Data.Argonaut (class DecodeJson, class EncodeJson, JObject, decodeJson, encodeJson, fromObject, getField, toObject)
+import Data.Argonaut.Core (stringify)
+import Data.Either (Either(..))
+import Data.Foreign (Foreign, toForeign)
+import Data.Foreign.Class (class AsForeign, write)
+import Data.Maybe (Maybe(..))
+import Data.Nullable (Nullable)
+import Prelude (Unit, unit, (>>>), bind, ($), pure, (<$>))
+
+foreign import data RTCPeerConnection :: *
+
+data ServerType
+  = STUN { url :: String }
+  | TURN { url :: String, credential :: String, username :: String }
+
+
+type Ice = { iceServers :: Array ServerType }
+
+instance iceAsForeign :: AsForeign ServerType where
+  write (STUN s) = toForeign s
+  write (TURN t) = toForeign t
+
+foreign import newRTCPeerConnection_
+  :: forall e. { iceServers :: Array Foreign } -> Eff e RTCPeerConnection
+
+newRTCPeerConnection :: forall e. Ice -> Eff e RTCPeerConnection
+newRTCPeerConnection i = newRTCPeerConnection_ { iceServers : (write <$> i.iceServers) }
+
+foreign import addStream
+  :: forall e. MediaStream -> RTCPeerConnection -> Eff e Unit
+
+foreign import data IceEvent :: *
+
+type RTCIceCandidate = { sdpMLineIndex :: Nullable Int
+                       , sdpMid :: Nullable String
+                       , candidate :: String
+                       }
+
+foreign import _iceEventCandidate
+  :: forall a. Maybe a ->
+               (a -> Maybe a) ->
+               IceEvent ->
+               Maybe RTCIceCandidate
+
+iceEventCandidate :: IceEvent -> Maybe RTCIceCandidate
+iceEventCandidate = _iceEventCandidate Nothing Just
+
+foreign import addIceCandidate
+  :: forall e. RTCIceCandidate ->
+               RTCPeerConnection ->
+               Eff e Unit
+
+foreign import onicecandidate
+  :: forall e. (IceEvent -> Eff e Unit) ->
+               RTCPeerConnection ->
+               Eff e Unit
+
+type MediaStreamEvent = { stream :: MediaStream }
+
+foreign import onaddstream
+  :: forall e. (MediaStreamEvent -> Eff e Unit) ->
+               RTCPeerConnection ->
+               Eff e Unit
+
+foreign import data RTCSessionDescription :: *
+
+foreign import rtcSessionDescriptionToJson :: RTCSessionDescription -> JObject
+
+rtcSessionDescriptionToString :: RTCSessionDescription -> String
+rtcSessionDescriptionToString = encodeJson >>> stringify
+
+instance rtcSessionDescriptionEncodeJson :: EncodeJson RTCSessionDescription where
+  encodeJson a = fromObject (rtcSessionDescriptionToJson a)
+
+instance rtcSessionDescriptionDecodeJson :: DecodeJson RTCSessionDescription where
+  decodeJson json = do
+    obj <- decodeJson json
+    sdp <- getField obj "sdp"
+    t <- getField obj "type"
+    pure $ newRTCSessionDescription { "sdp" : sdp, "type" : t }
+
+foreign import newRTCSessionDescription
+  :: { sdp :: String, "type" :: String } -> RTCSessionDescription
+
+foreign import _createOffer
+  :: forall e. (RTCSessionDescription -> Eff e Unit) ->
+               (Error -> Eff e Unit) ->
+               RTCPeerConnection ->
+               Eff e Unit
+
+createOffer :: forall e. RTCPeerConnection -> Aff e RTCSessionDescription
+createOffer pc = makeAff (\e s -> _createOffer s e pc)
+
+foreign import _createAnswer
+  :: forall e. (RTCSessionDescription -> Eff e Unit) ->
+               (Error -> Eff e Unit) ->
+               RTCPeerConnection ->
+               Eff e Unit
+
+createAnswer :: forall e. RTCPeerConnection -> Aff e RTCSessionDescription
+createAnswer pc = makeAff (\e s -> _createAnswer s e pc)
+
+foreign import _setLocalDescription
+  :: forall e. Eff e Unit ->
+               (Error -> Eff e Unit) ->
+               RTCSessionDescription ->
+               RTCPeerConnection ->
+               Eff e Unit
+
+setLocalDescription :: forall e. RTCSessionDescription -> RTCPeerConnection -> Aff e Unit
+setLocalDescription desc pc = makeAff (\e s -> _setLocalDescription (s unit) e desc pc)
+
+foreign import _setRemoteDescription
+  :: forall e. Eff e Unit ->
+               (Error -> Eff e Unit) ->
+               RTCSessionDescription ->
+               RTCPeerConnection ->
+               Eff e Unit
+
+setRemoteDescription :: forall e. RTCSessionDescription -> RTCPeerConnection -> Aff e Unit
+setRemoteDescription desc pc = makeAff (\e s -> _setRemoteDescription (s unit) e desc pc)
+
+foreign import data RTCDataChannel :: *
+
+foreign import createDataChannel
+  :: forall e. String ->
+               RTCPeerConnection ->
+               Eff e RTCDataChannel
+
+foreign import send
+  :: forall e. String ->
+               RTCDataChannel ->
+               Eff e Unit
+
+foreign import onmessageChannel
+  :: forall e. (String -> Eff e Unit) ->
+               RTCDataChannel ->
+               Eff e Unit
